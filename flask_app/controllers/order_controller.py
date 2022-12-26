4c069223afad29d3e30fccb94c3125fa96c75036
Tyler Thibault <tyler.thibault@protonmail.com>
2022-12-26 12:22:17 -0700

    fixed adding a day after one day has already been added. on main branch

--- flask_app/controllers/order_controller.py
+++ flask_app/controllers/order_controller.py
@@ -130,6 +130,8 @@ def order_update(id):
             all_orders_on_date = order_model.Order.get(delivery_date = data['delivery_date'])
             if all_orders_on_date:
                 config = config_model.Config.get(id = 1)
+                if type(all_orders_on_date) != type([]):
+                    all_orders_on_date = [all_orders_on_date]
                 if len(all_orders_on_date) >= config.max_daily_orders:
                     flash("Order quantity has execced bakers daily quantity, please find another date to have these items delivered. Thank you for your understanding.", "err_order_daily_max")
                     return redirect(last_page)
